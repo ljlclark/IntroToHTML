96f2f2d2040ab8dad40b3b2eb81d4e9b7ac86c78
ljlclark <35316538+ljlclark@users.noreply.github.com>
2025-04-28 08:29:02 +0800

    Update NavEvents.js

--- Script/NavEvents.js
+++ Script/NavEvents.js
@@ -6,6 +6,7 @@ class NavEvents
   // Initializes an object instance with the provided values.
   constructor(contentFrame)
   {
+    // Create the class properties.
     this.ContentFrame = contentFrame;
     this.PrevNavItem = null;
   }
@@ -19,56 +20,74 @@ class NavEvents
     menuIco.addEventListener("click", this.MenuClick.bind(this));
     content.addEventListener("mouseenter", this.ContentMouseEnter.bind(this));
 
+    // Create the class element properties.
     this.menubar = document.getElementById("menubar");
     this.menuIco = document.getElementById("menuIco");
     this.sidebar = document.getElementById("sidebar");
     this.content = document.getElementById("content");
+
+    // Set defaults.
     this.menubar.style.display = "none";
     this.menuIco.style.display = "none";
     this.reducedWidth = false;
     this.WindowResize();
   }
 
-  // 
+  // Set to reduced with if below minimum width.
   WindowResize()
   {
-    let width = window.innerWidth;  // Webpage with scrollbars
-    if (width < 800)
+    // Webpage width with scrollbars
+    let width = window.innerWidth;
+
+    let minimum = 800;
+    if (width < minimum)
     {
       this.reducedWidth = true;
+
+      // Show the menu icon.
       this.menubar.style.display = "block";
       this.menuIco.style.display = "block";
+
+      // Hide the sidebar
       this.sidebar.style.display = "none";
-      this.sidebar.style.width = "240px";  // use widest string width?
+      // ToDo: use widest string width?
+      this.sidebar.style.width = "240px";
       this.content.style.width = "100%";
     }
     else
     {
       this.reducedWidth = false;
+
+      // Hide the menu icon.
       this.menubar.style.display = "none";
       this.menuIco.style.display = "none";
+
+      // Show the sidebar
       this.sidebar.style.display = "inline-block";
       this.sidebar.style.position = "relative";
-      this.sidebar.style.width = "25%";  // use widest string width?
+      // ToDo: use widest string width?
+      this.sidebar.style.width = "25%";
       this.content.style.width = "75%";
     }
   }
 
-  // 
+  // Click on the menu icon.
   MenuClick()
   {
     if (this.sidebar.style.display == "none")
     {
+      // Show the sidebar.
       this.sidebar.style.display = "inline-block";
       this.sidebar.style.position = "absolute";
     }
     else
     {
+      // Hide the sidebar.
       this.sidebar.style.display = "none";
     }
   }
 
-  // 
+  // Hide the sidebar if at a reduced width.
   ContentMouseEnter()
   {
     if (this.reducedWidth == true)
@@ -78,13 +97,16 @@ class NavEvents
   }
 
   // Document "click" handler method.
-  // event - The Target event.
+  // event - The Source Element event.
   DocumentClick(event)
   {
     let srcElement = event.target;
+
+    // Only perform sidebar action for these class selectors.
     if ("navLiveGroup" == srcElement.className
       || "navItem" == srcElement.className)
     {
+      // Get the data-href value.
       let href = srcElement.dataset.href;
       if (href)
       {
@@ -97,10 +119,13 @@ class NavEvents
 
       if (this.PrevNavItem != null)
       {
+        // Resets to original value.
         this.PrevNavItem.style.backgroundColor = "";
       }
       this.PrevNavItem = srcElement;
-      srcElement.style.backgroundColor = "#d4dfff";
+
+      let highlight = "#d4dfff";
+      srcElement.style.backgroundColor = highlight;
     }
   }
 }
